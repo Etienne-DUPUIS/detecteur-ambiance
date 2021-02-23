0bd9ce83015701a118f52ffa87606b311f1d156d
edupuis <etienne.dupuis@ec-lyon.fr>
2021-02-23 01:17:40 +0100

    add humidity sensor

--- raspi/sensors/humidity.py
+++ raspi/sensors/humidity.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+import gpiozero
 import sys
 import Adafruit_DHT
+
 
 DHT_11 = 11
 PIN = 26
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_11, 6)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_11, PIN)
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
