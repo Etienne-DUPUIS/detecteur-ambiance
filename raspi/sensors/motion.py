cdcc3b5c207f9d7de3210dc1493f3dacaed2d41d
edupuis <etienne.dupuis@ec-lyon.fr>
2021-02-23 10:39:24 +0100

    Fixed wrong port

--- raspi/sensors/motion.py
+++ raspi/sensors/motion.py
@@ -3,24 +3,21 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-PIR_PIN = 13
+PIR_PIN = 19
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 print('Starting up the PIR Module (click on STOP to exit)')
 time.sleep(1)
-print('Ready')
+print('Ready-')
 
-i = 0
 
 def callback(pin):
-    global i
     if GPIO.input(pin):
-        print(i)
-        i += 1
+        print("Detecting Movement")
     else:
         print('O')
 
-GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(PIR_PIN, callback)  # assign function to GPIO PIN, Run function on change
 
 # infinite loop
