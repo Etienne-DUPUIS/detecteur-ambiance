89d3f3439c4f4dea4981f259aa460ad06806043c
edupuis <etienne.dupuis@ec-lyon.fr>
2021-02-23 14:46:01 +0100

    add dataset creation

--- raspi/dataset_creation.py
+++ raspi/dataset_creation.py
@@ -1,3 +1,39 @@
-import numpy as np
+import os
+import time
+
 import pandas as pd
 
+log_root = "log/"
+log_path = os.path.join(log_root, time.ctime() + ".csv")
+
+
+def write_data(df):
+    if not os.path.exists(log_root):
+        os.makedirs(log_root)
+    with open(log_path, 'w+') as file:
+        file.write(df.to_csv())
+    return log_path
+
+
+def record_data(timeout_s, sensor_read_callback, ids):
+    tic = time.time()
+    df = None
+    while time.time() - tic < timeout_s:
+        timestamp, data = sensor_read_callback()
+        df = append_data(
+            df, {
+                **{"timestamp": timestamp},
+                **{ids[i]: value for i, value in enumerate(data)}
+            }
+        )
+    return df
+
+
+def append_data(data, data_dict):
+    return pd.concat(
+        [
+            data, pd.DataFrame(
+            data_dict, index=[0]
+        )
+        ], ignore_index=True
+    )
--- raspi/sensor.py
+++ raspi/sensor.py
@@ -1,5 +1,6 @@
 from IO import humidity, motion, LCD
 from datetime import datetime
+import dataset_creation as ds
 
 
 class Sensor():
@@ -39,9 +40,16 @@ def read_all_sensor():
 
 
 if __name__ == "__main__":
+    print("Start recording data for 10s")
+    data = ds.record_data(10, read_all_sensor, ["Humidity", "Temperature", "Motion"])
+    print("Stop recording")
+    print(data)
+    print(ds.write_data(data))
+
     while True:
         timestamp, values = read_all_sensor()
         LCD.lcd_text(str(timestamp), LCD.LCD_LINE_1)
         LCD.lcd_text(str(values), LCD.LCD_LINE_2)
+
 
 
